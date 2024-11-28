9f1b3201b326ec82222857ebe3b2b0cac0008b8f
simasturbo07 <161766609+simasturbo07@users.noreply.github.com>
2024-11-28 14:59:43 -0300

    Add files via upload

---
+++ leonardo/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'O que é Penalti?',
+    'É um chute direto ao gol, de dentro da área menor, causada quando ocorre uma penalidade dentro dessa área'
+)
+
+criaCartao(
+    'Vôlei',
+    'Quando a bola cai em cima da linha no seu lado da quadra, é ponto adversário?',
+    'sim'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual a capital da Angola',
+    'Luanda'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
